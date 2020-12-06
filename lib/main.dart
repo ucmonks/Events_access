a4b438ff49446508a4fad66960c27701156a92cf
4bh1 <abhiram123vj@gmail.com>
2020-12-07 02:33:19 +0530

    Second Row Added

--- lib/main.dart
+++ lib/main.dart
@@ -15,59 +15,113 @@ class MyApp extends StatelessWidget {
           child: Column(
             children: [
               SizedBox(
-                height: 85,
+                height: 62,
               ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                
-                  Container(
-                      height: 18,
-                      child: Image.asset("assets/images/backico.png")),
-                  Container(
-                    child: Row(
-                      children: [
-                        SizedBox(
-                          width: 25,
-                        ),
-                        Container(
-                            height: 30,
-                            child: Image.asset("assets/images/search.png",
-                                fit: BoxFit.cover)),
-                        SizedBox(
-                          width: 10,
-                        ),
-                        Container(
-                          height: 50,
-                          width: 240,
-                          child: TextField(
-                            decoration: InputDecoration(
-                                border: InputBorder.none,
-                                hintText: 'Search',
-                                hintStyle: TextStyle(fontSize: 20)),
+              Container(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Container(
+                        height: 18,
+                        child: Image.asset("assets/images/backico.png")),
+                    Container(
+                      child: Row(
+                        children: [
+                          SizedBox(
+                            width: 25,
                           ),
-                        ),
-                        SizedBox(
-                          width: 5,
-                        )
-                      ],
-                    ),
-                    decoration: BoxDecoration(
-                        color: BackgrndCol,
-                        borderRadius: BorderRadius.circular(30)),
-                  )
-                ],
+                          Container(
+                              height: 30,
+                              child: Image.asset("assets/images/search.png",
+                                  fit: BoxFit.cover)),
+                          SizedBox(
+                            width: 10,
+                          ),
+                          Container(
+                            height: 50,
+                            width: 240,
+                            child: TextField(
+                              decoration: InputDecoration(
+                                  border: InputBorder.none,
+                                  hintText: 'Search',
+                                  hintStyle: TextStyle(fontSize: 20)),
+                            ),
+                          ),
+                          SizedBox(
+                            width: 5,
+                          )
+                        ],
+                      ),
+                      decoration: BoxDecoration(
+                          color: BackgrndCol,
+                          borderRadius: BorderRadius.circular(35)),
+                    )
+                  ],
+                ),
+                height: 90,
+                decoration: BoxDecoration(
+                  color: AppbarCol,
+                  borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(24),
+                    bottomRight: Radius.circular(24),
+                  ),
+                ),
               ),
+              SizedBox(
+                height: 20,
+              ),
+              SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                physics: BouncingScrollPhysics(),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    buildRaisedButtonTop("Hot dishes", isSelected: true),
+                    buildRaisedButtonTop("Salads"),
+                    buildRaisedButtonTop("Hot drink"),
+                    buildRaisedButtonTop("Cold drink"),
+                  ],
+                ),
+              )
             ],
           ),
-          height: 156,
-          decoration: BoxDecoration(
-              color: AppbarCol,
-              borderRadius: BorderRadius.only(
-                bottomLeft: Radius.circular(20),
-                bottomRight: Radius.circular(20),
-              )),
         ),
+      ),
+    );
+  }
+
+  Padding buildRaisedButtonTop(String txt, {bool isSelected = false}) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: RaisedButton(
+        textColor: isSelected ? Colors.white : Colors.grey,
+        padding: EdgeInsets.all(0.0),
+        shape: StadiumBorder(),
+        child: Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(30),
+            gradient: LinearGradient(
+              colors: isSelected
+                  ? <Color>[
+                      Color(0xFFe3b638),
+                      Color(0xFFe86637),
+                      Color(0xFFd239a1),
+                    ]
+                  : <Color>[
+                      Color(0xFFFFFFFF),
+                      Color(0xFFFFFFFF),
+                    ],
+            ),
+          ),
+          child: Text(
+            txt,
+            style: TextStyle(fontSize: 23,fontWeight: FontWeight.w400),
+          ),
+          padding: EdgeInsets.symmetric(horizontal: 28, vertical: 19.0),
+        ),
+        onPressed: () {
+          print("pressed $txt");
+        },
       ),
     );
   }
