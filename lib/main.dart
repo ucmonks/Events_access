0ef043a76693b9dbe71f20e95b9f8692cd087784
4bh1 <abhiram123vj@gmail.com>
2020-12-07 03:15:53 +0530

    cardViewAdded

--- lib/main.dart
+++ lib/main.dart
@@ -17,75 +17,93 @@ class MyApp extends StatelessWidget {
               SizedBox(
                 height: 62,
               ),
-              Container(
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    Container(
-                        height: 18,
-                        child: Image.asset("assets/images/backico.png")),
-                    Container(
-                      child: Row(
-                        children: [
-                          SizedBox(
-                            width: 25,
-                          ),
-                          Container(
-                              height: 30,
-                              child: Image.asset("assets/images/search.png",
-                                  fit: BoxFit.cover)),
-                          SizedBox(
-                            width: 10,
-                          ),
-                          Container(
-                            height: 50,
-                            width: 240,
-                            child: TextField(
-                              decoration: InputDecoration(
-                                  border: InputBorder.none,
-                                  hintText: 'Search',
-                                  hintStyle: TextStyle(fontSize: 20)),
-                            ),
-                          ),
-                          SizedBox(
-                            width: 5,
-                          )
-                        ],
-                      ),
-                      decoration: BoxDecoration(
-                          color: BackgrndCol,
-                          borderRadius: BorderRadius.circular(35)),
-                    )
-                  ],
-                ),
-                height: 90,
-                decoration: BoxDecoration(
-                  color: AppbarCol,
-                  borderRadius: BorderRadius.only(
-                    bottomLeft: Radius.circular(24),
-                    bottomRight: Radius.circular(24),
-                  ),
-                ),
-              ),
+              appBarContnt(),
               SizedBox(
                 height: 20,
               ),
-              SingleChildScrollView(
-                scrollDirection: Axis.horizontal,
-                physics: BouncingScrollPhysics(),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    buildRaisedButtonTop("Hot dishes", isSelected: true),
-                    buildRaisedButtonTop("Salads"),
-                    buildRaisedButtonTop("Hot drink"),
-                    buildRaisedButtonTop("Cold drink"),
-                  ],
+              scrollRow(),
+              SizedBox(
+                height: 20,
+              ),
+              Flexible(
+                child: GridView.count(
+                  crossAxisCount: 2,
+                  padding: EdgeInsets.all(16.0),
+                  childAspectRatio: 8.0 / 9.0,
+                  // TODO: Build a grid of cards (102)
+                  children: <Widget>[Card(),Card(),Card(),Card(),Card(),Card()],
                 ),
               )
             ],
           ),
         ),
+      ),
+    );
+  }
+
+  Container appBarContnt() {
+    return Container(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Container(
+              height: 18, child: Image.asset("assets/images/backico.png")),
+          Container(
+            child: Row(
+              children: [
+                SizedBox(
+                  width: 25,
+                ),
+                Container(
+                    height: 30,
+                    child: Image.asset("assets/images/search.png",
+                        fit: BoxFit.cover)),
+                SizedBox(
+                  width: 10,
+                ),
+                Container(
+                  height: 50,
+                  width: 240,
+                  child: TextField(
+                    decoration: InputDecoration(
+                        border: InputBorder.none,
+                        hintText: 'Search',
+                        hintStyle: TextStyle(fontSize: 20)),
+                  ),
+                ),
+                SizedBox(
+                  width: 5,
+                )
+              ],
+            ),
+            decoration: BoxDecoration(
+                color: BackgrndCol, borderRadius: BorderRadius.circular(35)),
+          )
+        ],
+      ),
+      height: 90,
+      decoration: BoxDecoration(
+        color: AppbarCol,
+        borderRadius: BorderRadius.only(
+          bottomLeft: Radius.circular(24),
+          bottomRight: Radius.circular(24),
+        ),
+      ),
+    );
+  }
+
+  SingleChildScrollView scrollRow() {
+    return SingleChildScrollView(
+      scrollDirection: Axis.horizontal,
+      physics: BouncingScrollPhysics(),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          buildRaisedButtonTop("Hot dishes", isSelected: true),
+          buildRaisedButtonTop("Salads"),
+          buildRaisedButtonTop("Hot drink"),
+          buildRaisedButtonTop("Cold drink"),
+        ],
       ),
     );
   }
@@ -115,7 +133,7 @@ class MyApp extends StatelessWidget {
           ),
           child: Text(
             txt,
-            style: TextStyle(fontSize: 23,fontWeight: FontWeight.w400),
+            style: TextStyle(fontSize: 23, fontWeight: FontWeight.w400),
           ),
           padding: EdgeInsets.symmetric(horizontal: 28, vertical: 19.0),
         ),
